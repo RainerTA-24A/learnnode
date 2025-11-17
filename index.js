5650103e69bd587fd518aa8ce28ea699db3de80f
Rainer Tahker <rainer.tahker@tptlive.ee>
2025-11-17 09:35:18 +0200

    Use import instead of require

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("Hello via Bun!");
 
-const common = require('./common.js');
+//const common = require('./common.js');
+import common from './common.js'
 common.hello();
 
 import es from './es.js';
