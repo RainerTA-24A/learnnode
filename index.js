99df689a911ea30a94943aa77d4ca7fdbf710a79
Rainer Tahker <rainer.tahker@tptlive.ee>
2025-12-01 09:11:12 +0200

    Add vite

--- index.js
+++
@@ -1,7 +0,0 @@
-//import fs from 'fs'; //fs = filesystem
-
-//fs.writeFileSync('test.txt', 'Hello file!');
-
-import boxen from 'boxen';
-
-console.log(boxen('unicorns love rainbows', { title: 'magical', titleAlignment: 'center', backgroundColor: 'cyan' }));
