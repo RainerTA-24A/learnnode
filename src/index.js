d15251bccbbda2f25ce2a5a72553ce4fbcbfac80
Rainer Tahker <rainer.tahker@tptlive.ee>
2026-01-21 12:33:53 +0200

    Made it so scss works also added background color.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue';
 // import the root component App from a single-file component.
 import App from './App.vue';
 
-const app = createApp(App);
-app.mount('#app')
+import './style.scss'
+
+createApp(App).mount('#app')
+//const app = createApp(App);
+//app.mount('#app')
