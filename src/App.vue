53dcf28ba9468255dad045277d7867f155097b28
Rainer Tahker <rainer.tahker@tptlive.ee>
2026-01-21 13:17:50 +0200

    Vue kasutamise viisid. reverse text example.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup>
-    let message = 'Hello Vue!';
+import { ref } from 'vue';
+
+let message = ref('Hello Vue!');
+
+/*function input(event){
+    console.log(event.target.value)
+    message.value = event.target.value
+  }*/
+
 </script>
 
 <template>
-    <h1>{{ message }}</h1>;
+    <div class="container">
+    <h1>{{ message.split('').reverse().join('') }}</h1>
+    <button class="button is-primary" @click=" message = 'This ez'">Click me</button> <!-- v-on: asemel @-->
+    <!--   <input/>   ise sulgev tag-->
+     <!--  <input class="input" :value="message" @input="message = $event.target.value">   v-on:input="input" siis peaks seda function input(event) kasutama-->	 
+    <!--   <input class="input" v-bind:value="message" v-on:change="console.log('change')" placeholder="change"> -->
+
+    <input class="input" v-model="message">
+    </div>
 </template>
 
 <style>
