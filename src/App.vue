917006115fecd2de165470a5384daf0e6a5cc642
Rainer Tahker <rainer.tahker@tptlive.ee>
2026-01-21 13:21:08 +0200

    Lisasin kommentaarid vue'st text faili

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,6 @@ import { ref } from 'vue';
 
 let message = ref('Hello Vue!');
 
-/*function input(event){
-    console.log(event.target.value)
-    message.value = event.target.value
-  }*/
 
 </script>
 
@@ -15,8 +11,6 @@ let message = ref('Hello Vue!');
     <h1>{{ message.split('').reverse().join('') }}</h1>
     <button class="button is-primary" @click=" message = 'This ez'">Click me</button> <!-- v-on: asemel @-->
     <!--   <input/>   ise sulgev tag-->
-     <!--  <input class="input" :value="message" @input="message = $event.target.value">   v-on:input="input" siis peaks seda function input(event) kasutama-->	 
-    <!--   <input class="input" v-bind:value="message" v-on:change="console.log('change')" placeholder="change"> -->
 
     <input class="input" v-model="message">
     </div>
