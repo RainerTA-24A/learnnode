5fa39894828c6ba5984e66e6f7cc83f6221a6e9a
Rainer Tahker <rainer.tahker@tptlive.ee>
2026-03-18 13:11:05 +0200

    Chuck Norris old way(XMLHTTP)

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import ToDo from "./pages/ToDo.vue";
 import Modals from "./pages/Modals.vue";
 import Messages from "./pages/Messages.vue";
 import Buttons from "./pages/Buttons.vue";
+import ChuckNorris from "./pages/ChuckNorris.vue";
 
 const routes = [
   { path: "/", component: ToDo, name: "ToDo_67" },
   { path: "/modals", component: Modals, name: "Modals_laud" },
   { path: "/messages", component: Messages, name: "Messages" },
   { path: "/buttons", component: Buttons, name: "Buttons" },
+  { path: "/chuck", component: ChuckNorris, name: 'Chuck Norris' },
 ];
 
 export const router = createRouter({
