e431296974e651eabee1fbecfc6cf3a0eda2bf59
RainerTahker <rainer.tahker@tptlive.ee>
2026-04-01 14:09:54 +0300

    Improve RickAndMorty / Pages

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -1,8 +1,7 @@
 <script setup>
-import axios from 'axios';
-import { ref } from 'vue';
-import CharacterCard from '../components/CharacterCard.vue';
-
+import axios from "axios";
+import { computed, ref } from "vue";
+import CharacterCard from "../components/CharacterCard.vue";
 
 let characters = ref([]);
 let pagination = ref({
@@ -10,36 +9,95 @@ let pagination = ref({
     pages: 0,
     next: null,
     prev: null,
-
 });
 
-await getCharacters('https://rickandmortyapi.com/api/character');
+let current = ref(1);
 
-async function getCharacters(url) {
-    let res = await axios.get(url);
+await getCharacters(current.value);
+
+async function getCharacters(page) {
+    current.value = page;
+    let res = await axios.get('https://rickandmortyapi.com/api/character', {
+        params: {
+            page: page
+        }
+    });
     console.log(res.data);
     characters.value = res.data.results;
     pagination.value = res.data.info;
-
 }
 
 async function next() {
-    await getCharacters(pagination.value.next);
-
+    await getCharacters(current.value + 1);
 }
 
 async function prev() {
-    await getCharacters(pagination.value.prev);
-
+    await getCharacters(current.value - 1);
 }
 
-</script>
 
+let pages = computed(() => {
+    let pages = [];
+    for (let i = 1; i <= 3; i++) {
+        pages[i] = i;
+    }
+    if (current.value > 6) {
+        pages.push("...");
+    }
+    for (
+        let i = current.value - 2;
+        i <= current.value + 2 && i <= pagination.value.pages;
+        i++
+    ) {
+        pages[i] = i;
+    }
+
+    if (current.value < pagination.value.pages - 2) {
+        pages.push("...");
+    }
+
+    for (let i = pagination.value.pages - 2; i <= pagination.value.pages; i++) {
+        pages[i] = i;
+    }
+
+    return pages.filter((p) => p);
+});
+</script>
 
 <template>
     <div class="container">
-        <button class="button is-primary" :disabled="!pagination.prev" @click="prev">Prev</button>
-        <button class="button is-primary" :disabled="!pagination.next" @click="next">Next</button>
+        <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+            <button class="pagination-previous" :disabled="!pagination.prev" @click="prev">
+                Previous
+            </button>
+            <button class="pagination-next" :disabled="!pagination.next" @click="next">
+                Next Page
+            </button>
+            <ul class="pagination-list">
+                <li v-for="page in pages">
+                    <span v-if="page === '...'" class="pagination-ellipsis">&hellip;</span>
+                    <button v-else-if="page === current" class="pagination-link is-current" aria-label=" Goto page 1">
+                        {{ page }}
+                    </button>
+                    <button v-else class="pagination-link" aria-label="Goto page 1" @click="getCharacters(page)">
+                        {{ page }}
+                    </button>
+                </li>
+
+                <!-- <li><button class="pagination-link" aria-label="Goto page 1">1</button></li>
+                <li><span class="pagination-ellipsis">&hellip;</span></li>
+                <li>
+                    <button class="pagination-link" aria-label="Goto page 45">45</button>
+                </li>
+                <li>
+                    <button class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</button>
+                </li>
+                <li><button class="pagination-link" aria-label="Goto page 47">47</button></li>
+                <li><span class="pagination-ellipsis">&hellip;</span></li>
+                <li><button class="pagination-link" aria-label="Goto page 86">86</button></li> -->
+            </ul>
+        </nav>
+
         <div class="columns is-multiline">
             <div class="column is-3" v-for="character in characters">
                 <CharacterCard :character="character"></CharacterCard>
